2ba0624c9a61b28b3a3770761b1fe5d6da571589
aniketzz <aniket.manoj@gmail.com>
2019-04-11 12:32:28 +0530

    Readme added

--- obj_detect_crop.py
+++ obj_detect_crop.py
@@ -8,7 +8,7 @@ execution_path = os.getcwd()
 
 i=1          ## if the file names are numeric and in sequence set i value to the 1st file name.  Eg: 1.jpg --> i = 1
 
-num = 50     ##defune the threshold for the coordinates to expand on each side(left, top,  )
+num = 25     ##defune the threshold for the coordinates to expand on each side(left, top,  )
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()         ##select yolo as a pre-trained model
@@ -17,7 +17,7 @@ detector.loadModel()
 
 while (i<=1):   ## range of the toltal files/images in the directory
 
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "frame641.jpg"), output_image_path=os.path.join(execution_path , "cnew0.png"), display_object_name = False, display_percentage_probability = False, minimum_percentage_probability=90)
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "original.jpg"), output_image_path=os.path.join(execution_path , "cnew0.png"), display_object_name = False, display_percentage_probability = False, minimum_percentage_probability=90)
     for eachObject in detections:
 
         print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
