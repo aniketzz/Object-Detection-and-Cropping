7c93920672bd443f9b117eec479479759d42f637
aniketzz <aniket.manoj@gmail.com>
2019-04-15 10:30:23 +0530

    Update obj_detect_crop.py

--- obj_detect_crop.py
+++ obj_detect_crop.py
@@ -10,14 +10,16 @@ i=1          ## if the file names are numeric and in sequence set i value to the
 
 num = 25     ##defune the threshold for the coordinates to expand on each side(left, top,  )
 
+path = "pictures/"   ##path to the input images
+
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()         ##select yolo as a pre-trained model
 detector.setModelPath( os.path.join(execution_path , "yolo.h5"))   ##path to the yolo weight file
 detector.loadModel()
 
 while (i<=1):   ## range of the toltal files/images in the directory
-
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "original.jpg"), output_image_path=os.path.join(execution_path , "cnew0.png"), display_object_name = False, display_percentage_probability = False, minimum_percentage_probability=90)
+    filename = path + "frame%d.jpg"%i
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , filename), output_image_path=os.path.join(execution_path , "output/frame%d.jpg"%i), display_object_name = False, display_percentage_probability = False, minimum_percentage_probability=90)
     for eachObject in detections:
 
         print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
@@ -34,9 +36,9 @@ while (i<=1):   ## range of the toltal files/images in the directory
             #print("------->"+str(cnew))
             #print (eachObject["name"])
 
-            img = Image.open("cnew0.png")
+            img = Image.open("output/frame%d.jpg"%i)
             image_new = img.crop(cnew)    ##crop the object
-            image_new.save("cnew1.png")   ##output path
+            image_new.save("output/frame%d.jpg"%i)   ##output path
         print("--------------------------------")
     i += 1
 
